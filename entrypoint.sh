4d05861daa0e4670015f699526966c3805ae4b67
SamMacy <49598357+SamMacy@users.noreply.github.com>
2021-08-05 16:24:16 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -103,11 +103,11 @@ topics=${tmp%]*}
 
 echo ${topics}
 
-if [ ${topics} =~ ${TOPIC1} ]; then
+if [ ${topics} == ${TOPIC1} ]; then
    echo "It's there1!"
 fi
 
-if [ ${topics} =~ ${TOPIC2} ]; then
+if [ ${topics} == ${TOPIC2} ]; then
    echo "It's there2!"
 fi
 curl \
