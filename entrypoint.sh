e4d5edd2de97263ca88286fea835a1ad25472803
SamMacy <49598357+SamMacy@users.noreply.github.com>
2021-08-05 14:07:44 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,7 +16,7 @@ if [ -z "$PROJECT2_URL" ] && [ "$PROJECT1_URL" ]; then
 fi
 
 if [ "$PROJECT2_URL" ] && [ "$PROJECT1_URL" ]; then
-  if [ "$TOPIC1" ] || [ "$TOPIC2" ]; then
+  if [ -z "$TOPIC1" ] || [ -z "$TOPIC2" ]; then
     echo "A topic is not defined." >&2
     exit 1
   fi
