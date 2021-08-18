1319a095cbdc9c589f451ea7d46acf9df1b25ff5
SamMacy <49598357+SamMacy@users.noreply.github.com>
2021-08-18 13:38:10 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,3 @@ TOKEN="$GITHUB_TOKEN"
 curl -s -X GET -u '$GITHUB_ACTOR:$TOKEN' --retry 3 \
   -H 'Accept: application/vnd.github.v3+json' \
   https://api.github.com/SamMacy/memberships/orgs
-  
-print("end")
