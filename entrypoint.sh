fb45e1f7e6ad269cc324ea52e37296f9ba0f3747
SamMacy <49598357+SamMacy@users.noreply.github.com>
2021-08-18 14:26:19 -0400

    Update and rename entrypoint.sh to Script.py

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -l
-
-TOKEN="$GITHUB_TOKEN"
-
-curl -s -X GET -u '$GITHUB_ACTOR:$TOKEN' --retry 3 \
-  -H 'Accept: application/vnd.github.v3+json' \
-  https://api.github.com/user/memberships/orgs
