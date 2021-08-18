eea964091d27196e078c4f3b1bcdcee53fc91455
SamMacy <49598357+SamMacy@users.noreply.github.com>
2021-08-18 13:32:38 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 TOKEN="$GITHUB_TOKEN" 
 
-print( $(curl -s -X GET -u "$GITHUB_ACTOR:$TOKEN" --retry 3 \
+print( $(curl -s -X GET -u '$GITHUB_ACTOR:$TOKEN' --retry 3 \
   -H 'Accept: application/vnd.github.v3+json' \
   https://api.github.com/orgs/Senzing/members) )
 
