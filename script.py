b422f815831cbc32c0c4ddfee79afb2f69d50364
SamMacy <49598357+SamMacy@users.noreply.github.com>
2021-08-20 14:55:18 -0400

    Update script.py

--- script.py
+++ script.py
@@ -4,16 +4,32 @@ import slack
 import os 
 
 slack_bearer_token = os.getenv('TOKEN')
-slack_channel = "Playground" 
+slack_channel = "Playground"
+repo = os.getenv('REPO_URL')
+issue_number = os.getenv('NUMBER')
+issue = repo + "issue/" + issue_number
+
+print(issue)
 print(os.getenv('BOOL'))
 print(os.getenv('CREATOR'))
 print(os.getenv('TITLE'))
-print(os.getenv('NUMBER'))
 slack_message = "test message"
+
+
 # Create a client that communicates with Slack.
 
 slack_client = slack.WebClient(token=slack_bearer_token)
 
+"blocks": [
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "<http://www.example.com|This message *is* a link>\n<mailto:bob@example.com|Email Bob Roberts>"
+			}
+		}
+	]
+  
 response = slack_client.chat_postMessage(
                 channel=slack_channel,
                 text=slack_message
