96f7b35dde4bf893cb65fd4bc89428a3c87acb3f
SamMacy <49598357+SamMacy@users.noreply.github.com>
2021-08-20 13:31:52 -0400

    Update script.py

--- script.py
+++ script.py
@@ -1,24 +1,9 @@
 #! /usr/bin/env python3
 
-import json
-import requests
+import slack
 
-slack_token = 'xoxb-my-bot-token'
-slack_channel = '#my-channel'
-slack_icon_emoji = ':see_no_evil:'
-slack_user_name = 'Double Images Monitor'
+slack_bearer_token = "$TOKEN"
+print(slack_bearer_token)
+# Create a client that communicates with Slack.
 
-def post_message_to_slack(text, blocks = None):
-    return requests.post('https://slack.com/api/chat.postMessage', {
-        'token': slack_token,
-        'channel': slack_channel,
-        'text': text,
-        'icon_emoji': slack_icon_emoji,
-        'username': slack_user_name,
-        'blocks': json.dumps(blocks) if blocks else None
-    }).json()	
-  
-    slack_info = 'There are *{}* double images detected for *{}* products. Please check the <https://{}.s3-eu-west-1.amazonaws.com/{}|Double Images Monitor>.'.format(
-    double_images_count, products_count, bucket_name, file_name)
-
-post_message_to_slack(slack_info)
+slack_client = slack.WebClient(token=slack_bearer_token)
