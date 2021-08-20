0170302296fa7c54287fd15d992846b9fe6e16ba
SamMacy <49598357+SamMacy@users.noreply.github.com>
2021-08-20 13:46:40 -0400

    Update script.py

--- script.py
+++ script.py
@@ -5,6 +5,7 @@ import os
 
 slack_bearer_token = os.getenv('TOKEN')
 print(slack_bearer_token)
+print(os.getenv('BOOL'))
 # Create a client that communicates with Slack.
 
 slack_client = slack.WebClient(token=slack_bearer_token)
